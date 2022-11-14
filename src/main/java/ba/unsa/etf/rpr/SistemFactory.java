e498b9a6c7bbf592ab6f2de6628f8e6b33b62c4b
Lejla Eminagić <leminagic1@etf.unsa.ba>
2022-11-14 14:33:50 +0100

    3.komit

--- src/main/java/ba/unsa/etf/rpr/SistemFactory.java
+++ src/main/java/ba/unsa/etf/rpr/SistemFactory.java
@@ -1,5 +1,13 @@
 package ba.unsa.etf.rpr;
 
 public class SistemFactory {
-
+    public Sistem dajSistem(String opis){
+        if(opis == null) return null;
+        else if(opis.equalsIgnoreCase("Android")){
+            return new Android();
+        }
+        else{
+            return new iOS();
+        }
+    }
 }
