ecf497e73ddfe1ac384c0325ce409be8dd3097a6
Lejla Eminagić <leminagic1@etf.unsa.ba>
2022-11-14 14:27:38 +0100

    Initial commit

---
+++ src/main/java/ba/unsa/etf/rpr/Android.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr;
+
+public class Android implements Sistem {
+    @Override
+    public void opisSistema() {
+        System.out.println("Sistem je otvoren!");
+    }
+}
---
+++ src/main/java/ba/unsa/etf/rpr/iOS.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr;
+
+public class iOS implements Sistem{
+    @Override
+    public void opisSistema() {
+        System.out.println("Sistem je zatvoren!");
+    }
+}
